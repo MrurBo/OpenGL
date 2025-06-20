b2dab5ac021ac6e630356b1dfb52356eb3018583
Einemillarde2005120303 <ishan.mishra@stu.hetss.ie>
2025-06-20 17:13:27 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hello World
+
+## This is some code:
+
+```cpp
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World";
+
+    return 0;
+}
+```
+
+blah blah blah...
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit()) return -1;
+
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) std::cerr << "GLEW not OK!" << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.0f, 0.5f,
+        0.5f, -0.5f
+    };
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
