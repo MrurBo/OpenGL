4a3daf10feb3dc0e20a2815d66867f94322a8fa6
Einemillarde2005120303 <ishan.mishra@stu.hetss.ie>
2025-06-28 20:09:28 +0100

    ~

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-/*
-  Credit to https://learnopengl.com/Getting-started/Hello-Window for the tutorial
+/**
+ * Credit to https://learnopengl.com/Getting-started/Hello-Window for the tutorial
 */
 
 #include <glad/glad.h>
@@ -25,8 +25,8 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height)
 
 void update(GLFWwindow* window)
 {
-  /*
-    glfwGetKey` returns 1 if pressed else 0
+  /**
+   * glfwGetKey` returns 1 if pressed else 0
   */
 
   if (glfwGetKey(window, GLFW_KEY_ESCAPE)) glfwSetWindowShouldClose(window, true);
@@ -79,8 +79,8 @@ unsigned int compileShader(unsigned int type, std::string source)
 unsigned int createProgram(unsigned int vertexShader, unsigned int fragmentShader)
 {
     // Create shader program
-  /*
-    A shader program links all the shaders together to help them communicate with each other
+  /**
+   * A shader program links all the shaders together to help them communicate with each other
   */
   unsigned int shaderProgram = glCreateProgram();
 
@@ -145,10 +145,10 @@ int main() {
   // Define vertices
   // Each vertex has 5 attributes : x, y, z, u, v
   float vertices[] = {
-    -.5f, -.5f, 0, 0, 0,
-    -.5f, .5f, 0, 0, 1,
-    .5f, .5f, 0, 1, 1,
-    .5f, -.5f, 0, 1, 0
+    -.25f, -.25f, 0, 0, 0,
+    -.25f, .25f, 0, 0, 1,
+    .25f, .25f, 0, 1, 1,
+    .25f, -.25f, 0, 1, 0
   };
 
   unsigned int indices[] = {
@@ -166,33 +166,33 @@ int main() {
   glBindVertexArray(vao);
 
   // Bind buffers to its buffer type
-  /*
-    OpenGL allows you to bind multiple buffers as long as they have separate buffer types
+  /**
+   * OpenGL allows you to bind multiple buffers as long as they have separate buffer types
   */
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 
   // Copies the data to the buffer bound to the its respective buffer type
-  /*
-    first argument: type of the buffer we want to copy data into
-    second argument: size of the data in bytes
-    third argument: data
-    fourth argument: specifies the graphics card how you want to manage the data
-      - GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
-      - GL_STATIC_DRAW: the data is set only once and used many times.
-      - GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+  /**
+   * first argument: type of the buffer we want to copy data into
+   * second argument: size of the data in bytes
+   * third argument: data
+   * fourth argument: specifies the graphics card how you want to manage the data
+   *   - GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+   *   - GL_STATIC_DRAW: the data is set only once and used many times.
+   *   - GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
   */
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   // Tells OpenGL the layout of out vertex data
-  /*
-    first argument: location of the vertex attribute array we want to configure
-    second argument: number of values in the vertex attribute
-    third argument: data type
-    forth argument: whether the values should be normalized
-    fifth argument: the space between the start of two consecutive vertex attributes in the array in bytes (also known as stride)
-    sixth argument: offset of where the position data begins (it has to be cast into a void pointer for some reason, idk why)
+  /**
+   * first argument: location of the vertex attribute array we want to configure
+   * second argument: number of values in the vertex attribute
+   * third argument: data type
+   * forth argument: whether the values should be normalized
+   * fifth argument: the space between the start of two consecutive vertex attributes in the array in bytes (also known as stride)
+   * sixth argument: offset of where the position data begins (it has to be cast into a void pointer for some reason, idk why)
   */
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
@@ -204,10 +204,10 @@ int main() {
   glGenTextures(1, &texture);
 
   // Clamp the texture so that anything outside the texture will be a user defined color
-  /*
-    first argument: texture target (21d, 2d, 3d, etc...)
-    second argument: which axis we are configuring (axes: s, t, r(if using 3d textures))
-    third argument: texture wrapping mode we would like
+  /**
+   * first argument: texture target (21d, 2d, 3d, etc...)
+   * second argument: which axis we are configuring (axes: s, t, r(if using 3d textures))
+   * third argument: texture wrapping mode we would like
   */
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
@@ -217,10 +217,10 @@ int main() {
   glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
   // Specify the filtering mode
-  /*
-    the filtering mode the color is at a point on the texture relative to the colors of the four texels that surround it
-    nearest neighbour or point filtering returns the color of the nearest texel
-    bilinear filtering linearly interpolates between the four colors to get a ratiod mix of the four
+  /**
+   * the filtering mode the color is at a point on the texture relative to the colors of the four texels that surround it
+   * nearest neighbour or point filtering returns the color of the nearest texel
+   * bilinear filtering linearly interpolates between the four colors to get a ratiod mix of the four
   */
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -265,35 +265,59 @@ int main() {
       // Initialize transform matrix
       glm::mat4 trans = glm::mat4(1);
 
+      /**
+       * A hard thing to get used to in OpenGL is that the last transformation you write in code is applied first
+       * This is because matrices are multiplied from right to left
+       * 
+       * Say you applied a rotation, then a translation,
+       * the program writes it down as:
+       * --------------------------------------------------------------------------------------------------------
+       * | transform_matrx = R * T
+       * --------------------------------------------------------------------------------------------------------
+       * and if you multiply that with a vector, it becomes:
+       * --------------------------------------------------------------------------------------------------------
+       * | vector = R * T * v
+       * --------------------------------------------------------------------------------------------------------
+       * ... which is equal to ...
+       * --------------------------------------------------------------------------------------------------------
+       * | R * (T * v)
+       * --------------------------------------------------------------------------------------------------------
+       * 
+       * It's hard to remember, but that's just how it is `\_('_')_/`
+       */
+
+      // Translate transform matrix
+      trans = glm::translate(trans, glm::vec3(std::sin(glfwGetTime() * 1.5) / 2, 0.0, 0.0));
+
       // Rotate transform matrix
       trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0, 0, 1));
 
       // Create uniform for that matrix
-      /*
-        first argument: uniform location
-        second argument: how many matrices we would like to send
-        third argument: if we would like to transpose out matrix (swap columns and rows)
-        fourth argument: convert to OpenGL format
-      */
+      /**
+       * first argument: uniform location
+       * second argument: how many matrices we would like to send
+       * third argument: if we would like to transpose out matrix (swap columns and rows)
+       * fourth argument: convert to OpenGL format
+       */
       glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "u_transformMatrix"), 1, GL_FALSE, glm::value_ptr(trans));
 
       // Draw Vertex Arrays
-      /*
-        frist argument: the type of primitive we would like to draw
-        second argument: the index of the vertex array
-        third argument: the number of vertices to draw
-      */
+      /**
+       * frist argument: the type of primitive we would like to draw
+       * second argument: the index of the vertex array
+       * third argument: the number of vertices to draw
+       */
       glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // Swaps front and back buffers
-    /*
-      An application takes time to draw all the pixels on the screen
-      When an application draws on a single buffer, it can have a flickering effect as not all pixels have been drawn when the screen renders
-      To solve this issue, applications implement double buffers
-      The front buffer contains the final output image shown on the screen
-      All the rendering commands draw to the back buffer
-      As soon as the back buffer is complete, they swap, instantaneously changing frames without flickering
-    */
+    /**
+     * An application takes time to draw all the pixels on the screen
+     * When an application draws on a single buffer, it can have a flickering effect as not all pixels have been drawn when the screen renders
+     * To solve this issue, applications implement double buffers
+     * The front buffer contains the final output image shown on the screen
+     * All the rendering commands draw to the back buffer
+     * As soon as the back buffer is complete, they swap, instantaneously changing frames without flickering
+     */
     glfwSwapBuffers(window);
 
     // Checks if any events are triggered (keyboard, mouse, etc)
